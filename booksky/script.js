3994cc7f1af90f7c06dd33e68c727ccc1650da92
Chandru-Sivakumar <144266172+Chandru-Sivakumar@users.noreply.github.com>
2024-07-26 15:05:47 +0530

    Add files via upload

---
+++ booksky/script.js
@@ -0,0 +1,8 @@
+var pop = document.querySelector(".pop")
+var popbg = document.querySelector(".pop-bg")
+var addbutton = document.querySelector(".add-button")
+
+addbutton.addEventListener("click", function(){
+    pop.style.display="block";
+    popbg.style.display="block";
+})
